e18cc6011b3498539d315b765ee929adcfa417b8
전선일 <jeonsunil1225@gamil.com>
2025-06-03 22:51:57 +0900

    Feat: 시작 페이지 설정

--- lol_simulation/src/app/page.tsx
+++ lol_simulation/src/app/page.tsx
@@ -1,7 +1,35 @@
-export default function Home() {
+'use client'
 
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  
+  const router = useRouter()
+
+  const handleClick = () => {router.push('/select')}
+
+
+  
   return (
     <>
+      <p className="text-5xl 
+                    text-center
+                    ">
+                    LCK 선수 15$ 챌린지
+                    </p>
+      <button className="
+              cursor-pointer 
+              px-[100px] 
+              py-[30px] 
+              border 
+              rounded-[10px]
+              border-black
+              bg-gray-100
+              hover:bg-gray-500
+              "
+              onClick={handleClick}>
+              시작하기
+              </button>
     </>
   );
 }
