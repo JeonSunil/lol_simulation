42288017909cd2ba9a80003143905d197e38c19e
전선일 <jeonsunil1225@gamil.com>
2025-06-03 22:51:44 +0900

    Feat: 사이트 타이틀 및 설명 변경

--- lol_simulation/src/app/layout.tsx
+++ lol_simulation/src/app/layout.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  title: "lck선수 15달러 챌린지",
+  description: "lck선수 15달러 챌린지",
 };
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
